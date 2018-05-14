360faaea23ffcca2700b12958363f6912dfcbd61
white <booleanwhite@gmail.com>
2018-05-14 16:18:09 +0800

    first commit

---
+++ app/src/main/java/com/white/www/modeltest/MyModel.java
@@ -0,0 +1,12 @@
+package com.white.www.modeltest;
+
+/**
+ * Created by White on 2018/5/14.
+ */
+
+public class MyModel {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
